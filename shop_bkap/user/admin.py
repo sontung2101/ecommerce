85bf12de27d17725f1125dcc137935d59b8977a1
abc <sontung2101@gmail.com>
2019-12-14 12:43:13 +0700

    modifile user.model and user.form

--- shop_bkap/user/admin.py
+++ shop_bkap/user/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from user.models import CustomerUser
+from django.contrib.auth.admin import UserAdmin
+
 # Register your models here.
-admin.site.register(CustomerUser)
+admin.site.register(CustomerUser, UserAdmin)
--- shop_bkap/user/form.py
+++ shop_bkap/user/form.py
@@ -1,15 +1,30 @@
-from django import forms
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from user.models import CustomerUser
-
-
-class CustomUserCreationForm(UserCreationForm):
-    class Meta:
-        model = CustomerUser
-        fields = "__all__"
-
-
-class CustomUserChangeForm(UserChangeForm):
-    class Meta:
-        model = CustomerUser
-        fields = "__all__"
+# from django import forms
+# from django.contrib.auth.models import User
+# from django.contrib.auth.forms import UserCreationForm
+#
+#
+# # Create your models here.
+# class allusers1(UserCreationForm):
+#     full_name = forms.CharField(label="full_name", max_length=10)
+#     phone_number = forms.CharField(label="phone_number")
+#
+#     class Meta:
+#         model = User
+#         fields = (
+#             'username',
+#             'password',
+#             'full_name',
+#             'phone_number',
+#         )
+#
+#     def save(self, commit=True):
+#         user = super(allusers1, self).save(commit=False)
+#         user.username1 = self.cleaned_data['username']
+#         user.password1 = self.cleaned_data['password']
+#         user.phone_number = self.cleaned_data['phone_number']
+#         user.full_name = self.cleaned_data['full_name']
+#
+#         if commit:
+#             user.save()
+#
+#         return user
